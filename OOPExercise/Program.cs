06d4d016ac518cf5542484d9372ae664615e99f8
Zsolt Kardos <zsolt.kardos@hotmail.hu>
2021-01-09 11:39:49 +0100

    Add global variables

--- OOPExercise/Program.cs
+++ OOPExercise/Program.cs
@@ -1,9 +1,15 @@
-﻿using System;
+﻿using OOPExercise_Console.Models;
+using System;
+using System.Collections.Generic;
 
 namespace OOPExercise
 {
     class Program
     {
+        private static List<IShape> shapes = new List<IShape>();
+        private static List<Type> types = new List<Type>() { typeof(Rectangle), typeof(Triangle) };
+        private static string menuItem = "";
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
